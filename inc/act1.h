b56187dcb12cdb2e8ad465b34b061e1483482d3d
Ankana9910 <68809362+Ankana9910@users.noreply.github.com>
2021-04-27 17:04:48 +0530

    Update act1.h

--- inc/act1.h
+++ inc/act1.h
@@ -1,1 +1,21 @@
+#ifdef _ACT1_H_
+#define _ACT1_H_
+/**
+ * @file main.c
+ * @author Ankana ()
+ * @brief
+ * @version 0.1
+ * @date 2021-04-27
+ * @copyright Copyright (c) 2021
+ *
+ */
+/**
+ * Macro Definitions
+ */
 
+
+#define LED_PORT (PORTB)    /**< LED Port Number */
+#define LED_PIN  (PORTB0)   /**< LED Pin number  */
+
+
+#endif /* _ACT1_H_ */
